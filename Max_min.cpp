5051bbc3132cc727fc95c0cde82ef8802b369033
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-04 16:36:02 +0530

    commit

---
+++ Max_min.cpp
@@ -0,0 +1,20 @@
+// this program is to find weather the number is maximum or minimum
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"This program is to find weather the number is max or min";
+    cout<<"\nenter first value";
+    cin>>a;
+    cout<<"\nenter Second value";
+    cin>>b;
+    if(a>b){
+        cout<<"\nA is Max"; 
+    }
+    else if(b>a){
+            cout<<"\nB is Max";
+    }
+    else{
+        cout<<"Both are equal";
+    }
+}
