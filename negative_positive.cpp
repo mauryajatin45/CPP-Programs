bb7ecebf7fbfd6da36e64bc44b7c6c1f70fa920b
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-04 16:27:02 +0530

    push

---
+++ negative_positive.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+cout<<"Enter any number to find weather its negative or positive-->";
+cin>>a;
+if(a>=0){
+ cout<<"The number is positive";
+}
+else{
+    cout<<"The number is Negative";
+}
+}
