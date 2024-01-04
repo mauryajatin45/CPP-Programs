349dd77423fd801de7a73970bd0c8a0f2b2c4d5d
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-04 10:01:18 +0530

    push

---
+++ Square_cube.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter any number to find the Square and cube:-";
+    cin>>a;
+    cout<<"\nSquare is "<<a*a;
+    cout<<"\ncube is "<<a*a*a;
+}
