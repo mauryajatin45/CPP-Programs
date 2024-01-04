8ce29d39d051c1db2cea908212484126d033017d
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-04 08:41:40 +0530

    Commit

---
+++ if_else.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter your age:-";
+    cin>>a;
+    if(a<18){
+        cout<<"you are not eligilable for licience";
+    }
+    else
+    {
+        cout<<"\nyou are eligilable for licience";
+    }
+}
---
+++ pyramid.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int a=1;
+    while (a, a <= 5)
+    {
+        cout << "Hello World";
+        cout<<"\n";
+        a++;
+    }
+}
