d108f8e8319bdc5b7e1158da192a6880e37d0ccd
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-05 08:37:20 +0530

    commit

---
+++ Table.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"Enter any number to find the table of it";
+    cin>>a;
+    for(b=1; b<=10; b++){
+        cout<<a<<"x"<<b<<"="<<a*b<<"\n";
+    }
+}
