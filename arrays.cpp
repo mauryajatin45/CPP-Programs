e4f920f0ac85a37fdb45a03d0e19c63c667af877
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-06 08:43:50 +0530

    Commit

--- arrays.cpp
+++ arrays.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-
-
+int main()
+{
+    int arr[5] = {5, 10, 12, 17, 0};
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+    cout << arr[4] << endl;
     return 0;
 }
