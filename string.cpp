8c9cf4cceb8da6dd712849f69a202cf801d3775b
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-07 09:40:39 +0530

    Commit

---
+++ string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a;
+    a = "Jatin";
+    cout<<"The output of String is "<<a<<endl;
+    cout<<"The output of String is "<<a.length()<<endl;
+    cout<<"The output of String is "<<a.substr(0,2)<<endl;
+}
