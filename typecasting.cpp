dc149e1e97bed0e2e96b794bc43452d6ef44dac1
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-07 09:26:50 +0530

    Commit

---
+++ typecasting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void div(){
+    int a, b;
+    cout<<"Enter first Value-->";
+    cin>>a;
+    cout<<"Enter second Value-->";
+    cin>>b;
+    cout<<"Answer of Division is-->"<<(float)a/b;
+}
+int main(){
+    // This program is to do the functions of type casting
+    div();
+}
