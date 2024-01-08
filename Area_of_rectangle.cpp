ea337991dac3ff54874a553b59eb3b1302bf3660
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-08 17:38:27 +0530

    commit

---
+++ Area_of_rectangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l, b;
+    cout<<"Enter the length"<<endl;
+    cin>>l;
+    cout<<"Enter the Breadth"<<endl;
+    cin>>b;
+    cout<<"The area of rectangle = "<< l * b;
+}
---
+++ Area_of_square.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter length of a side to find the area of square --> ";
+    cin>>a;
+    cout<<"The area = "<<a * a;
+}
