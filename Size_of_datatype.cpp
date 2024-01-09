7d56610897e39b5f90de04c364f4c773cb181402
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-10 04:43:49 +0530

    Commit

--- Size_of_datatype.cpp
+++ Size_of_datatype.cpp
@@ -2,4 +2,12 @@
 using namespace std;
 int main(){
     cout<<"The sixe of (Char)  "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"The sixe of (bool)  "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"The sixe of (short)  "<<sizeof(short)<<" bytes"<<endl;
+    cout<<"The sixe of (int)  "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"The sixe of (long)  "<<sizeof(long)<<" bytes"<<endl;
+    cout<<"The sixe of (float)  "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"The sixe of (long long)  "<<sizeof(long long)<<" bytes"<<endl;
+    cout<<"The sixe of (double)  "<<sizeof(double)<<" bytes"<<endl;
+    cout<<"The sixe of (long double)  "<<sizeof(long double)<<" bytes"<<endl;
 }
