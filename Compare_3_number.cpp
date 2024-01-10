e9691badb3c507ba3c65ad4e0673e8e09de019a9
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-10 09:27:43 +0530

    Commit

--- Compare_3_number.cpp
+++ Compare_3_number.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
+    int a, b, c;
+    cout << "This progeram will find the Greatest number between the three"<<endl;
+    cout << "Enter first value-->"<<endl;
+    cin >> a;
+    cout << "Enter second value-->"<<endl;
+    cin >> b;
+    cout << "Enter third value-->"<<endl;
+    cin >> c;
     // This Program is to check which number is greater and which one is Greatest among the Given Three Number
-    int a = 4;
-    int b = 5;
-    int c = 6;
+
     if (a > b)
     {
         if (a > c)
         {
-            {
-                cout << "A is greatest";
-            }
-            else
-            {
-                cout << "C is greater";
-            }
+            cout << "A is greatest";
         }
         else
         {
-            if (a < c)
-            {
-                cout << "C is greatest";
-            }
-            if (b > c)
-            {
-                cout << "B is greatest";
-            }
+            cout << "C is greater";
         }
     }
+    else
+    {
+        if (b < c)
+        {
+            cout << "B is greatest";
+        }
+        else
+        {
+            cout << "C is greatest";
+        }
+    }
+
     return 0;
 }
