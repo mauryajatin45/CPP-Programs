2acf13ec24b7e7b57e5e83322ebbf8e8384a1352
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-12 05:06:59 +0530

    Commit

---
+++ While loop/table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // this program will print the table of the number you want
+    int co;
+    cout<<"Enter a number of which you want to find the table of"<<endl;
+    cin>>co;
+    int a=1;
+    while (a, a<=10)
+    {
+        cout<<co<<"x"<<a<<"="<<co*a<<endl;
+        a++;
+    }
+
+}
