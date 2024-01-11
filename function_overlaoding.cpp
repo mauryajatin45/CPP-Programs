c557d0ebb90c8a506935f2cfa3a40eafa22a7cfb
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-11 09:06:39 +0530

    Commuit

--- function_overlaoding.cpp
+++ function_overlaoding.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
+void add(int a, int b){
+    cout<<"Add="<<a+b<<endl;
+}
+void add(int a, int b, int c){
+    cout<<"Add="<<a+b+c<<endl;
+}
 int main(){
-    
+add(22, 24);
+add(2, 2, 2);
 }
