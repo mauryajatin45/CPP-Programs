e84e10578d474bdbc1e34b5539ef703aabdb9407
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-11 08:55:40 +0530

    commit

---
+++ global_and_local_variable.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int a=10;
+int main(){
+int a=5;
+cout<<"The addition of a + a ="<<a + ::a ;
+// In this program the :: is used to take the value from the gloval declared variable.
+}
