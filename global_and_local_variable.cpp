dc1898d1a53832f1c09b1362d4c0742b935ffe63
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-11 09:01:39 +0530

    commit

--- global_and_local_variable.cpp
+++ global_and_local_variable.cpp
@@ -5,4 +5,5 @@ int main(){
 int a=5;
 cout<<"The addition of a + a ="<<a + ::a ;
 // In this program the :: is used to take the value from the gloval declared variable.
+// This is also known as scope variable
 }
