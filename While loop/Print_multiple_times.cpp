ccb6e9022be77c754f2b4c26b9186647669514ca
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-12 05:30:08 +0530

    commit

---
+++ While loop/Print_multiple_times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   cout<<"Enter any amount you want to loop this code"<<endl;
+    int i =1;
+    int j;
+    cin>>j;
+    while (i <= j)
+    {
+        cout << i << " I will be regular in class"<<endl;
+        i++;
+    }
+}
