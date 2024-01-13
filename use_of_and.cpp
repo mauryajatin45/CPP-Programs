6986f2509eb0d2c0489d2619acbc337543ffde4a
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-13 08:44:40 +0530

    Update use_of_and.cpp

--- use_of_and.cpp
+++ use_of_and.cpp
@@ -4,22 +4,22 @@ int main()
 {
     // Thid program is to illustrate the example of how to use && operator in c++.
     int a, b, c;
-    cout << " Enter first value-->"<<endl;
+    cout << " Enter first value-->" << endl;
     cin >> a;
-    cout << " Enter second value-->"<<endl;
+    cout << " Enter second value-->" << endl;
     cin >> b;
-    cout << " Enter third value-->"<<endl;
+    cout << " Enter third value-->" << endl;
     cin >> c;
     if ((a > b) && (a > c))
     {
-        cout << a<<" is the greatest"<<endl;
+        cout << a << " is the greatest" << endl;
     }
     else if ((b > a) && (b > c))
     {
-        cout << b<<" is the greatest"<<endl;
+        cout << b << " is the greatest" << endl;
     }
     else
     {
-        cout << c<<" is the greatest"<<endl;
+        cout << c << " is the greatest" << endl;
     }
 }
