a481e664f362df527cfde47f4091cfe9c052080c
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-18 09:41:33 +0530

    commit

---
+++ Even_number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // This program is to print The even numbers
+    int a =2;
+    for ( a; a<=50; a+=2)     {
+        cout<<a<<endl;
+    }
+    
+}
