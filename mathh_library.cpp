f0699d1f62d70f940503176190a7dff84532ea24
mauryajatin45 <mauryajatin45@gmail.com>
2024-01-18 09:59:54 +0530

    maths function

---
+++ mathh_library.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a =2;
+    cout<<"Sin = "<<sin(a)<<endl;
+    cout<<"cos = "<<cos(a)<<endl;
+    cout<<"tan = "<<tan(a)<<endl;
+    cout<<"Square root = "<<sqrt(a)<<endl;
+    cout<<"power = "<<pow(2,3)<<endl;
+}
